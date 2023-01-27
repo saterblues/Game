d7936e1e6e2323b04247588f79610574aeaf6cda
saterblues <80666621@qq.com>
2023-01-27 18:56:35 +0800

    Add files via upload

--- Entity/Entity.cs
+++ Entity/Entity.cs
@@ -12,15 +12,31 @@ namespace Game
         public Entity() {
             _guid = Guid.NewGuid();
         }
+
+        public Entity(Guid guid) {
+            _guid = guid;
+        }
         private Guid _guid;
         public Guid Uid
         {
-            set {
+            private set {
                 _guid = value;
             }
             get {
                 return _guid;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType()) { return false; }
+            Entity entity = (Entity)obj;
+            return entity._guid.Equals(this._guid);
+        }
+
+        public override int GetHashCode()
+        {
+            return _guid.GetHashCode();
         }
 
         public override string ToString()
